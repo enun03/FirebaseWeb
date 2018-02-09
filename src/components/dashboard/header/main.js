c743b2824f23539f9b939f8879fc115231eba6f7
NunezLopez <xcs4766@pwus.us>
2018-02-09 17:06:14 -0400

    login header in progress

--- src/components/dashboard/header/main.js
+++ src/components/dashboard/header/main.js
@@ -1,13 +1,23 @@
-(function(){
-  Header.render();
-})()
-
+(function() {
 var Header = {
-  element: document.querySelector('#header'),
-  render: function() {
-
-  },
+  el: document.querySelector('.header'),
+  _user: undefined,
   data: function() {
-
+    firebase.auth().onAuthStateChanged(function(user) {
+        if (user) {
+          // User is signed in.
+          Header._user = user;
+        }
+      });
+  },
+  render: function() {
+    this.data();
+    console.log(Header._user);
+    this.el.innerHTML = `
+      <h1>Welcome ${Header._user.displayName}!</h1>
+    `;
   }
 }
+
+Header.render();
+})()
