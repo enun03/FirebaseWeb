2b0683fb97d46b8d58250f27f5c8d81bcbb03968
Eli Nunez <enunez025@gmail.com>
2018-02-16 20:09:50 +0000

    adding more login options

--- src/firebase/Auth.js
+++ src/firebase/Auth.js
@@ -22,12 +22,13 @@
     signInSuccessUrl: '/dashboard.html',
     signInOptions: [
       // Leave the lines as is for the providers you want to offer your users.
+      firebase.auth.EmailAuthProvider.PROVIDER_ID,
       // firebase.auth.GoogleAuthProvider.PROVIDER_ID,
-      //firebase.auth.FacebookAuthProvider.PROVIDER_ID,
+      firebase.auth.FacebookAuthProvider.PROVIDER_ID,
       // firebase.auth.TwitterAuthProvider.PROVIDER_ID,
       //firebase.auth.PhoneAuthProvider.PROVIDER_ID,
-      //firebase.auth.GithubAuthProvider.PROVIDER_ID,
-      firebase.auth.EmailAuthProvider.PROVIDER_ID
+      firebase.auth.GithubAuthProvider.PROVIDER_ID
+      
     ],
     // Terms of service url.
     tosUrl: '/service-terms.html'
